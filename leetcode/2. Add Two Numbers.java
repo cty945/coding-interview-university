9c252437e44ad8753d2674a46bd934ce86362741
Tianyu Chen <tchen@exegy.com>
2020-12-23 00:15:26 -0600

    day2 leetcode

---
+++ leetcode/2. Add Two Numbers.java
@@ -0,0 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Scratch {
+    public static void main(String[] args) {
+
+        System.out.println("123");
+
+        Solution s = new Solution();
+//        s.addTwoNumbers();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        int addone = 0;
+
+        while (l1 != null || l2 != null || addone > 0){
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int sum = v1 + v2 + addone;
+            cur.next = new ListNode(sum % 10);
+            addone = sum / 10;
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (l1 != null){
+            cur.next = l1;
+        }
+        if (l2 != null){
+            cur.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
